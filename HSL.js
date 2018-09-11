65397dbc486345a3a3a10fc1c8d0a764013b40ed
MarioRozic <rozic.mario@hotmail.com>
2018-09-11 22:42:01 +0200

    :tada: Delete commets

--- HSL.js
+++ HSL.js
@@ -11,9 +11,6 @@ class HSL {
 }
 
 let hsl = new HSL({ r: 100, g: 200, b: 300 })
+
 console.log(hsl.color);
 console.log(hsl.r);
-// [a, b, c] = [10, 20, 300];
-// bzv, samo tipkamo za sad ispisuje samo tu klasu
-
-// sta ovo sad radi ?
